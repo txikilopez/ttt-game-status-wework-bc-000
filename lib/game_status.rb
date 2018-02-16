50523fcd3875a4c4eef15ab466bcf63dfb19d0eb
Antonio Troncoso <antonio.troncoso@wework.com>
2018-02-16 01:23:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,59 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =  [
+  [0,1,2], #row 1
+  [3,4,5], #row 2
+  [6,7,8], #row 3
+  [2,4,6], #diagonal 2
+  [0,4,8], #diagonal 1
+  [0,3,6], #column 1
+  [1,4,7], #column 2
+  [2,5,8], #column 3
+]
+  
+#FULL BOARD CODE
+def full?(board)
+  !(board.any? {|slot| slot == " " || slot == ""})
+end
+
+#WON CODE
+def won?(board)
+  
+  winning_move = nil
+  
+  WIN_COMBINATIONS.each do |move_win|
+    position_1 = move_win[0]
+    position_2 = move_win[1]
+    position_3 = move_win[2]
+    
+    if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
+      winning_move = move_win
+    elsif board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
+      winning_move = move_win
+    end
+  end
+  winning_move
+end
+
+#DRAW CODE
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+#OVER CODE
+def over?(board)
+  won?(board) || draw?(board) || full?(board)  || turn_count(board) == 9
+end
+
+#WINNER CODE
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
+
+#turn count
+def turn_count(board)
+  board.count{|x| x !=" "}
+end
